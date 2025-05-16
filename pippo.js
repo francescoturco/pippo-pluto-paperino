7c0dd554cb3d6480e992b8444593de937fe3ef4c
fratur <francescoturco2795@gmail.com>
2025-05-16 17:50:52 +0200

    exercise-done

--- pippo.js
+++ pippo.js
@@ -1,1 +1,23 @@
 console.log('Pippo-Pluto-Paperino')
+
+
+//A partire da un array di stringhe, crea un secondo array formattando le stringhe del primo array in minuscolo e con l'iniziale maiuscola.
+//Es: ['pippo', 'PLUTO', 'Paperino'] => ['Pippo', 'Pluto', 'Paperino']
+
+
+//ARRAY dei nomi
+const nomi = ['pippo', 'PLUTO', 'Paperino']
+console.log(nomi)
+
+const nomiEsatti = []; // OGGETTO VUOTO
+
+
+// uso il ciclo for per trasformare inizialmente tutto in minuscolo  e dopo dirgli di metterre la prima lettera in maiuscolo e il resto in minuscolo 
+
+for (let i = 0; i < nomi.length; i++) {
+    const nameSingle = nomi[i].toLowerCase();  // trasforma tutti i nomi in minuscolo 
+    const correctionName = nameSingle[0].toUpperCase() + nameSingle.slice(1);   //trasformo prima lettera del nome in maiuscolo e indico con slice il resto delle lettere della parola 
+    nomiEsatti.push(correctionName);  // push dell'oggetto inizialmente vuoto di nomi esatti con la correzzione
+}
+
+console.log (nomiEsatti)  //evoco nomi esatti corretti

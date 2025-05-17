55bfa9f52dd590465a44fdb60f3914a415b2a50b
fratur <francescoturco2795@gmail.com>
2025-05-17 09:37:52 +0200

    change-charAt-19

--- pippo.js
+++ pippo.js
@@ -15,8 +15,8 @@ const nomiEsatti = []; // OGGETTO VUOTO
 // uso il ciclo for per trasformare inizialmente tutto in minuscolo  e dopo dirgli di metterre la prima lettera in maiuscolo e il resto in minuscolo 
 
 for (let i = 0; i < nomi.length; i++) {
-    const nameSingle = nomi[i].toLowerCase();  // trasforma tutti i nomi in minuscolo 
-    const correctionName = nameSingle[0].toUpperCase() + nameSingle.slice(1);   //trasformo prima lettera del nome in maiuscolo e indico con slice il resto delle lettere della parola 
+    const nome = nomi[i].toLowerCase();  // trasforma tutti i nomi in minuscolo 
+    const correctionName = nome.charAt(0).toUpperCase() + nome.slice(1);   //trasformo prima lettera del nome in maiuscolo e indico con slice il resto delle lettere della parola 
     nomiEsatti.push(correctionName);  // push dell'oggetto inizialmente vuoto di nomi esatti con la correzzione
 }
 
